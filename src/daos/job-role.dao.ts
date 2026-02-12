fa817a3e3cacb9c8b429fac06351e188845ff2d4
SamTougherKainos <samuel.tougher@Kainos.com>
2026-02-12 11:23:40 +0000

    consistant await for dao applied

--- src/daos/job-role.dao.ts
+++ src/daos/job-role.dao.ts
@@ -1,19 +1,9 @@
 import type { JobRole } from "../generated/prisma/client";
 import { prisma } from "./prisma";
 
-export interface Capability {
-	capabilityId: number;
-	capabilityName: string;
-}
-
-export interface Band {
-	nameId: number;
-	bandName: string;
-}
-
 export class JobRoleDao {
 	async getOpenJobRoles(): Promise<JobRole[]> {
-		return await prisma.jobRole.findMany({
+		return prisma.jobRole.findMany({
 			include: {
 				capability: true,
 				band: true,
