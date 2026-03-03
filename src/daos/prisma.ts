7e02fd2bf4a91e6e79b971bd8db8c08dc6713d89
DavidOHanlon1985 <david.o'hanlon@kainos.com>
2026-03-03 15:38:03 +0000

    Formatting Complete

--- src/daos/prisma.ts
+++ src/daos/prisma.ts
@@ -3,7 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const databaseUrl = process.env.DATABASE_URL;
-const connectionString = databaseUrl?.replace(/@localhost:/g, "@host.docker.internal:");
+const connectionString = databaseUrl?.replace(
+	/@localhost:/g,
+	"@host.docker.internal:",
+);
 const schemaName = process.env.SCHEMA_NAME;
 
 const adapter = new PrismaPg({ connectionString }, { schema: schemaName });
